4e25afc77775bf2ea2757f4e10d9dd57937ec3b4
Tomas-Pacheco18 <trillex942@gmail.com>
2024-10-30 11:09:38 -0100

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 
-    int NumerosPares[20] = {,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    int NumerosPares[20] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 
  for(int i = 0; i <= 20; i++){
     if (NumerosPares[i] % 2 == 0){
